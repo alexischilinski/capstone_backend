9c8cc18a75c5b2d088512d82e9c60ce721588cff
alexiscait142 <chilinski.a@gmail.com>
2020-05-13 16:27:18 -0400

    refactor models

--- running/models.py
+++ running/models.py
@@ -3,43 +3,163 @@ from django.contrib.auth.models import User
 
 
 class Activity(models.Model):
-    race = models.CharField(max_length=200, null=True, blank=True)
-    week = models.IntegerField(null=True, blank=True)
-    day = models.IntegerField(null=True, blank=True)
-    workout_type = models.CharField(max_length=200, null=True, blank=True)
-    distance = models.CharField(max_length=200, null=True, blank=True)
+    race = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
+    week = models.IntegerField(
+        null=True, 
+        blank=True
+    )
+    day = models.IntegerField(
+        null=True, 
+        blank=True
+    )
+    workout_type = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
+    distance = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
 
 class Schedule(models.Model):
-    user = models.ForeignKey(User, related_name="schedules", on_delete=models.CASCADE, null=True, blank=True)
-    distance = models.CharField(max_length=200, null=True, blank=True)
-    race_name = models.CharField(max_length=200, null=True, blank=True)
-    duration = models.CharField(max_length=200, null=True, blank=True)
-    completed = models.BooleanField(null=True, blank=True)
+    user = models.ForeignKey(
+        User, 
+        related_name="schedules", 
+        on_delete=models.CASCADE, 
+        null=True, 
+        blank=True
+    )
+    distance = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
+    race_name = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
+    duration = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
+    completed = models.BooleanField(
+        null=True, 
+        blank=True
+    )
 
 class Workout(models.Model):
-    user = models.ForeignKey(User, related_name="workouts", on_delete=models.CASCADE, null=True, blank=True)
-    race = models.CharField(max_length=200, null=True, blank=True)
-    week = models.IntegerField(null=True, blank=True)
-    day = models.IntegerField(null=True, blank=True)
-    workout_type = models.CharField(max_length=200, null=True, blank=True)
-    distance = models.CharField(max_length=200, null=True, blank=True)
-    pace = models.CharField(max_length=200, null=True, blank=True)
-    duration = models.CharField(max_length=200, null=True, blank=True)
-    location = models.CharField(max_length=200, null=True, blank=True)
-    race_name = models.CharField(max_length=200, null=True, blank=True)
+    user = models.ForeignKey(
+        User, 
+        related_name="workouts", 
+        on_delete=models.CASCADE, 
+        null=True, 
+        blank=True
+    )
+    race = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
+    week = models.IntegerField(
+        null=True, 
+        blank=True
+    )
+    day = models.IntegerField(
+        null=True, 
+        blank=True
+    )
+    workout_type = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
+    distance = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
+    pace = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
+    duration = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
+    location = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
+    race_name = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
 
 class Friend(models.Model):
-    follower = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
-    following = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
+    follower = models.ForeignKey(
+        User, 
+        related_name="following", 
+        on_delete=models.CASCADE
+    )
+    following = models.ForeignKey(
+        User, 
+        related_name="followers", 
+        on_delete=models.CASCADE
+    )
 
 class Photo(models.Model):
-    user = models.ForeignKey(User, related_name="photo", on_delete=models.CASCADE, null=True, blank=True)
-    photo = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(
+        User, 
+        related_name="photo", 
+        on_delete=models.CASCADE, 
+        null=True, 
+        blank=True
+    )
+    photo = models.TextField(
+        null=True, 
+        blank=True
+    )
 
 class Message(models.Model):
-    receiver = models.ForeignKey(User, related_name="incoming", on_delete=models.CASCADE, null=True, blank=True)
-    sender = models.ForeignKey(User, related_name="outgoing", on_delete=models.CASCADE, null=True, blank=True)
-    message = models.TextField(blank=True, null=True)
-    read = models.BooleanField(blank=True, null=True)
-    subject = models.CharField(max_length=200, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    receiver = models.ForeignKey(
+        User, 
+        related_name="incoming", 
+        on_delete=models.CASCADE, 
+        null=True, 
+        blank=True
+    )
+    sender = models.ForeignKey(
+        User, 
+        related_name="outgoing", 
+        on_delete=models.CASCADE, 
+        null=True, 
+        blank=True
+    )
+    message = models.TextField(
+        blank=True, 
+        null=True
+    )
+    read = models.BooleanField(
+        blank=True, 
+        null=True
+    )
+    subject = models.CharField(
+        max_length=200, 
+        null=True, 
+        blank=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
